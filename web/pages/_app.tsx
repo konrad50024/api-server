1177ef7d2eb182dac449c4948579a52b3aae8e45
Anthony Shew <anthonyshew@gmail.com>
2021-12-19 17:13:29 -0700

    Help from my friends. :)

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useRouter } from "next/router"
+// import { Provider } from "react-redux"
+// import { store, useDispatch } from "@Redux/store"
+// import initFirebase from "@Services/firebase/index"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ReactQueryDevtools } from "react-query/devtools"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// initFirebase()
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 0
+		}
+	}
+})
+
+const AppWrap = ({ Component, pageProps }: any) => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{/* <Provider store={store}> */}
+			<MyApp Component={Component} pageProps={pageProps} />
+			{/* </Provider> */}
+		</QueryClientProvider>
+	)
 }
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: any) => {
+	const router = useRouter()
+	// const dispatch = useDispatch()
+
+	return (
+		<>
+			<Component {...pageProps} />
+			{/* <ReactQueryDevtools /> */}
+		</>
+	)
+}
+
+export default AppWrap
